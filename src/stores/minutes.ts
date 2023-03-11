0baa5e2a27c63f09bd65611f121ff487c6bf46a4
Jay <bitinbyte@outlook.com>
2023-03-11 19:40:40 +0100

    Added persistence

--- src/stores/minutes.ts
+++ src/stores/minutes.ts
@@ -1,42 +1,47 @@
 import { defineStore } from 'pinia'
 
 export const useMinutesStore = defineStore('minutes', {
-    state: () =>
+  state: () =>
     ({
-        minutes: [],
-        inputValue: ''
+      minutes: localStorage.getItem('minutes') ? JSON.parse(localStorage.getItem('minutes')!) : [],
+      inputValue: localStorage.getItem('minutes')
+        ? (JSON.parse(localStorage.getItem('minutes')!) as number[]).join(' ')
+        : ''
     } as {
-        minutes: number[]
-        inputValue: string
+      minutes: number[]
+      inputValue: string
     }),
-    actions: {
-        addMinutes(newValues: string) {
-            const isNumber = /^(?=.*\d)[\d ]+$/.test(newValues)
+  actions: {
+    addMinutes(newValues: string) {
+      const isNumber = /^(?=.*\d)[\d ]+$/.test(newValues)
 
-            if (isNumber) {
-                this.minutes = newValues
-                    .trim()
-                    .split(' ')
-                    .map((item) => +item)
-                this.inputValue = newValues
-            } else if (newValues === '') {
-                this.minutes = []
-                this.inputValue = newValues
-            }
-        },
-        removeMinute(index: number) {
-            this.minutes.splice(index, 1)
-            this.inputValue = this.minutes.join(' ')
-        }
+      if (isNumber) {
+        this.minutes = newValues
+          .trim()
+          .split(' ')
+          .map((item) => +item)
+        this.inputValue = newValues
+        localStorage.setItem('minutes', JSON.stringify(this.minutes))
+      } else if (newValues === '') {
+        this.minutes = []
+        this.inputValue = newValues
+        localStorage.setItem('minutes', JSON.stringify(this.minutes))
+      }
     },
-    getters: {
-        totalTime(): { hours: number; minutes: string } {
-            const totalMinutes = this.minutes.reduce((a, b) => a + b, 0)
-
-            const hours = Math.floor(totalMinutes / 60)
-            const minutes = totalMinutes % 60
-
-            return { hours, minutes: String(minutes).padStart(2, '0') }
-        }
+    removeMinute(index: number) {
+      this.minutes.splice(index, 1)
+      this.inputValue = this.minutes.join(' ')
+      localStorage.setItem('minutes', JSON.stringify(this.minutes))
     }
+  },
+  getters: {
+    totalTime(): { hours: number; minutes: string } {
+      const totalMinutes = this.minutes.reduce((a, b) => a + b, 0)
+
+      const hours = Math.floor(totalMinutes / 60)
+      const minutes = totalMinutes % 60
+
+      return { hours, minutes: String(minutes).padStart(2, '0') }
+    }
+  }
 })
